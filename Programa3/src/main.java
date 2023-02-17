ae7a5bbb5e2e1497ed071ca93481572d9d9bcc0f
JuanJoseSalesAlcon <124946527+JuanJoseSalesAlcon@users.noreply.github.com>
2023-02-16 19:36:18 -0600

    Add files via upload

---
+++ Programa3/src/main.java
@@ -0,0 +1,30 @@
+
+import Controladores.usuariosController;
+import Modelos.usuariosModel;
+import Vistas.frmPrincipal;
+import Vistas.frmUsuarios;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author USUARIO 2
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frmPrincipal VistaPrincipal = new frmPrincipal();
+        frmUsuarios VistaUsuarios = new frmUsuarios(VistaPrincipal,true);
+        usuariosModel ModeloUsuarios = new usuariosModel();
+        
+        usuariosController ControladorUsuarios = new usuariosController(VistaPrincipal,VistaUsuarios,ModeloUsuarios);
+    }
+    
+}
